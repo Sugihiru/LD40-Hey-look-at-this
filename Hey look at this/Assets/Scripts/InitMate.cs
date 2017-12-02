6d9b4139664f6125487b2b9343bfd27a51c37097
Cédric Khin <Cdric Khin>
2017-12-02 16:23:00 +0100

    mates moves toward seat

--- Hey look at this/Assets/Scripts/InitMate.cs
+++ Hey look at this/Assets/Scripts/InitMate.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InitMate : MonoBehaviour
 {
     public int TimeBeforeMovingToComputer;
+    public int MvtSpd;
+    private GameObject seat = null;
+    private bool seated = false;
 
 	// Use this for initialization
 	void Start ()
@@ -16,8 +18,17 @@ public class InitMate : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        if (seat && transform.position == seat.transform.position && !seated)
+        {
+            seated = true;
+            transform.rotation = Quaternion.LookRotation(Vector3.forward);
+        }
+        if (seat && !seated)
+        {
+            float step = MvtSpd * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, seat.transform.position, step);
+        }
+    }
 
     IEnumerator ExecuteAfterTime(float time)
     {
@@ -27,11 +38,12 @@ public class InitMate : MonoBehaviour
 
     private void MoveToComputer()
     {
-        Debug.Log(GameObject.FindGameObjectsWithTag("Mate").Length);
-
-        GameObject seat = GameObject.Find("Seats/Seat1");
+        seat = GameObject.Find("Seats/Seat" + GameObject.FindGameObjectsWithTag("Mate").Length);
         if (!seat)
+        {
             Debug.Log("Seat not found");
-        transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("Seats/Seat1").transform.position, 100);
+            return;
+        }
+        transform.rotation = Quaternion.FromToRotation(seat.transform.position, transform.position);
     }
 }
