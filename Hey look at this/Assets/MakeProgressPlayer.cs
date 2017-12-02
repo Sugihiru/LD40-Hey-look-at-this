a07bf6787dde6fa07ead982a88ff9d18c8edb3d8
Cédric Khin <Cdric Khin>
2017-12-02 17:54:35 +0100

    Progress bar

---
+++ Hey look at this/Assets/MakeProgressPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeProgressPlayer : MonoBehaviour
+{
+    private int Value = 0;
+    public int PtsWonByTime;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKey("space"))
+        {
+            Value += PtsWonByTime;
+            Debug.Log(Value);
+        }
+    }
+}
---
+++ Hey look at this/Assets/Progress.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Progress : MonoBehaviour
+{
+    private float Value = 0;
+    public float MaxValue;
+    public float PtsWonByTime;
+    private Image Img;
+
+    void Start()
+    {
+        Img = gameObject.GetComponent<Image>(); 
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("space") && Value < MaxValue)
+        {
+            Value += PtsWonByTime;
+            Debug.Log(Value / MaxValue);
+            Img.fillAmount = Value / MaxValue;
+        }
+    }
+}
