cab8c7b2a2162f6fa169b03be32f21cf8106cd07
Cédric Khin <Cdric Khin>
2017-12-02 22:07:12 +0100

    Timer text

---
+++ Hey look at this/Assets/IncreaseTimer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class IncreaseTimer : MonoBehaviour
+{
+    public float MaxValue;
+    public float PtsLostPerSecond;
+    public Text TimerTxt;
+
+    private float Value = 0;
+    private Image Img;
+    private TimeSpan timeSpan = new TimeSpan(0, 10, 0);
+
+    // Use this for initialization
+    void Start()
+    {
+        Img = gameObject.GetComponent<Image>();
+        UpdateDate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Value < MaxValue)
+        {
+            Value += PtsLostPerSecond * Time.deltaTime;
+            Img.fillAmount = Value / MaxValue;
+
+            TimeSpan span = TimeSpan.FromSeconds(Time.deltaTime);
+            timeSpan -= span;
+            UpdateDate();
+        }
+    }
+
+    private void UpdateDate()
+    {
+        if (timeSpan.Seconds >= 0)
+            TimerTxt.text = string.Format(@"{0:00}:{1:00}", timeSpan.Minutes, timeSpan.Seconds);
+    }
+}
