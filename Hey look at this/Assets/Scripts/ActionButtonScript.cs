35f8dc2aa20b6b82e10ec8e46b65bbeba5fffa3c
Cédric Khin <Cdric Khin>
2017-12-03 19:34:04 +0100

    Popup image

---
+++ Hey look at this/Assets/Scripts/ActionButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionButtonScript : MonoBehaviour
+{
+    private GameObject TypingGameWin;
+
+    public enum ActionDuration
+    {
+        FAST,
+        MEDIUM,
+        LONG
+    };
+
+    public void Start()
+    {
+        TypingGameWin = GameObject.Find("Canvas/TypingGameWindow");
+    }
+
+    public void OnFastActionButtonClick(ActionDuration actionDuration)
+    {
+        TypingGameWin.SetActive(true);
+        return;
+    }
+
+
+    public void OnActionButtonClick()
+    {
+        TypingGameWin.SetActive(true);
+        return;
+    }
+}
