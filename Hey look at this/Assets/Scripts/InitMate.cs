2ca8584ba72975c10e62c13b6ceefbdca7cfe705
Cédric Khin <Cdric Khin>
2017-12-03 12:25:55 +0100

    popup

--- Hey look at this/Assets/Scripts/InitMate.cs
+++ Hey look at this/Assets/Scripts/InitMate.cs
@@ -1,24 +1,29 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class InitMate : MonoBehaviour
 {
     public int TimeBeforeMovingToComputer;
     public int MvtSpd;
+    public GameObject SpeechBubble;
+
     private GameObject seat = null;
     private bool seated = false;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-        // Say hello
+        SayHello();
         GameObject button = GameObject.Find("ActionSidebar/MateActionsButtons/MateButton" + GameObject.FindGameObjectsWithTag("Mate").Length);
         button.SetActive(true);
         StartCoroutine(ExecuteAfterTime(TimeBeforeMovingToComputer));
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void Update()
     {
         if (seat && transform.position == seat.transform.position && !seated)
         {
@@ -47,5 +52,28 @@ public class InitMate : MonoBehaviour
             return;
         }
         transform.rotation = Quaternion.FromToRotation(seat.transform.position, transform.position);
+    }
+
+    private void SayHello()
+    {
+        Vector3 bubblePosition = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
+
+        GameObject bubbleGameObject = Instantiate(SpeechBubble, bubblePosition, transform.rotation);
+        Text textGameObject = bubbleGameObject.transform.GetChild(1).gameObject.GetComponent<Text>();
+        SetHelloTextOnId(textGameObject);
+
+        bubbleGameObject.transform.SetParent(transform);
+    }
+
+    private void SetHelloTextOnId(Text textGameObject)
+    {
+        int nbOtherMate = GameObject.FindGameObjectsWithTag("Mate").Length;
+        var HelloSentences = new string[]
+        {
+            "yo bois", "hey dude", "wassup", "gimme money",
+            "hey u, you look funny", "d'you have some food ?", "is it the cat fanclub ?", "Hey ! It's me, Mister Riichi!"
+        };
+
+        textGameObject.text = HelloSentences[nbOtherMate - 1];
     }
 }
