a2802e5454a7ea6c03a5c1a0c4bdf2deb9cd48dd
Cédric Khin <Cdric Khin>
2017-12-05 00:45:31 +0100

    Improved calculator input

--- Hey look at this/Assets/Minigames/Scripts/Calculator.cs
+++ Hey look at this/Assets/Minigames/Scripts/Calculator.cs
@@ -4,85 +4,104 @@ using UnityEngine.EventSystems;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Calculator : MonoBehaviour {
-	public bool over {get;set;}
-	public bool mustRestart {get;set;}
+public class Calculator : MonoBehaviour
+{
+    public bool over { get; set; }
+    public bool mustRestart { get; set; }
 
 
-	private Text operation;
-	private InputField answer;
-	private int expected;
-	private int nbr1;
-	private int nbr2;
-	private int operationType;
-	private int input;
-	private int nbrSuccess;
-	private int requiredSuccess;
-	private int difficulty;
+    private Text operation;
+    private InputField answer;
+    private int expected;
+    private int nbr1;
+    private int nbr2;
+    private int operationType;
+    private int input;
+    private int nbrSuccess;
+    private int requiredSuccess;
+    private int difficulty;
     private int MateId;
 
     // Use this for initialization
-    void Start () {
-		gameObject.transform.GetChild(0).gameObject.GetComponent<Text>().text = "Dude, help me, what's the answer ?";
-		requiredSuccess = 5 * (difficulty + 1);
-		nbrSuccess = 0;
-		mustRestart = true;
-		answer = gameObject.transform.GetChild(2).gameObject.GetComponent<InputField>();
-		operation = gameObject.transform.GetChild(1).gameObject.GetComponent<Text>();
-	}
+    void Start()
+    {
+        gameObject.transform.GetChild(0).gameObject.GetComponent<Text>().text = "Dude, help me, what's the answer ?";
+        requiredSuccess = 5 * (difficulty + 1);
+        nbrSuccess = 0;
+        mustRestart = true;
+        answer = gameObject.transform.GetChild(2).gameObject.GetComponent<InputField>();
+        operation = gameObject.transform.GetChild(1).gameObject.GetComponent<Text>();
+    }
 
-	public void Reset(int difficulty, int mateId)
+    public void Reset(int difficulty, int mateId)
     {
         MateId = mateId;
-		Start();
-		over = false;
-		gameObject.SetActive(true);
-		this.difficulty = difficulty;
-	}
+        Start();
+        over = false;
+        gameObject.SetActive(true);
+        this.difficulty = difficulty;
+        SetFocusOnInputField();
+    }
 
-	// Update is called once per frame
-	void Update () {
-		if (mustRestart) {
-			operationType = Random.Range(0, 2);
-			nbr2 = Random.Range(1, 100);
-			nbr1 = Random.Range(1, 100);
-			if (operationType == 0) {
-				operation.text = nbr1.ToString() + " + " + nbr2.ToString();
-				expected = nbr1 + nbr2;
-			}
-			if (operationType == 1) {
-				operation.text = nbr1.ToString() + " - " + nbr2.ToString();
-				expected = nbr1 - nbr2;
-			}
-			mustRestart = false;
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        if (mustRestart)
+        {
+            operationType = Random.Range(0, 2);
+            nbr2 = Random.Range(1, 100);
+            nbr1 = Random.Range(1, 100);
+            if (operationType == 0)
+            {
+                operation.text = nbr1.ToString() + " + " + nbr2.ToString();
+                expected = nbr1 + nbr2;
+            }
+            if (operationType == 1)
+            {
+                operation.text = nbr1.ToString() + " - " + nbr2.ToString();
+                expected = nbr1 - nbr2;
+            }
+            mustRestart = false;
+        }
 
+        if (!over && answer.text != "")
+        {
+            try
+            {
+                input = int.Parse(answer.text);
+            }
+            catch (System.FormatException)
+            {
+                return;
+            }
+            if (Input.GetKeyUp(KeyCode.Return) || Input.GetKeyUp(KeyCode.KeypadEnter))
+            {
+                answer.text = "";
 
-		if (! over && answer.text != "") {
-			try {
-					input = int.Parse(answer.text);
-			} catch (System.FormatException) {
-					return;
-			}
+                if (input != expected)
+                    return;
 
-			if (input == expected) {
-				nbrSuccess += 1;
-				answer.text = "";
-				if (nbrSuccess == requiredSuccess) {
-					over = true;
-					gameObject.SetActive(false);
-				}
-				else {
-					mustRestart = true;
-				}
-			}
-			else if (Input.GetKeyUp(KeyCode.Return) || Input.GetKeyUp(KeyCode.KeypadEnter)) {
-				answer.text = "";
-				EventSystem.current.SetSelectedGameObject(answer.gameObject, null);
-				answer.OnPointerClick(new PointerEventData(EventSystem.current));
-			}
-		}
-	}
+                // Correct input
+                nbrSuccess += 1;
+                if (nbrSuccess == requiredSuccess)
+                {
+                    over = true;
+                    gameObject.SetActive(false);
+                }
+                else
+                {
+                    mustRestart = true;
+                }
+            }
+        }
+        SetFocusOnInputField();
+    }
+
+    private void SetFocusOnInputField()
+    {
+        EventSystem.current.SetSelectedGameObject(answer.gameObject, null);
+        answer.OnPointerClick(new PointerEventData(EventSystem.current));
+    }
 
     void OnDisable()
     {
--- Hey look at this/Assets/Minigames/Scripts/Typer.cs
+++ Hey look at this/Assets/Minigames/Scripts/Typer.cs
@@ -7,44 +7,44 @@ public class Typer : MonoBehaviour
     public float nbPts;
 
     private Progress progress;
-	private bool over {get;set;}
+    private bool over { get; set; }
 
-	private Text display;
+    private Text display;
     private List<string> to_type = new List<string>();
-	private int word_offset;
-	private string result;
-	private string[] words = {
-		"for",
-		"each",
-		"in",
-		"while",
-		"len",
-		"if",
-		"elif",
-		"type",
-		"def",
-		"else",
-		"is",
-		"continue",
-		"not",
-		"class",
-		"and",
-		"as",
-		"assert",
-		"break",
-		"except",
-		"finally",
-		"from",
-		"global",
-		"import",
-		"lambda",
-		"or",
-		"pass",
-		"raise",
-		"return",
-		"try",
-		"with",
-		"yield",
+    private int word_offset;
+    private string result;
+    private string[] words = {
+        "for",
+        "each",
+        "in",
+        "while",
+        "len",
+        "if",
+        "elif",
+        "type",
+        "def",
+        "else",
+        "is",
+        "continue",
+        "not",
+        "class",
+        "and",
+        "as",
+        "assert",
+        "break",
+        "except",
+        "finally",
+        "from",
+        "global",
+        "import",
+        "lambda",
+        "or",
+        "pass",
+        "raise",
+        "return",
+        "try",
+        "with",
+        "yield",
         "enumerate",
         "print",
         "int",
@@ -53,27 +53,27 @@ public class Typer : MonoBehaviour
         "open",
         "range",
         "zip",
-	};
-	private int difficulty;
+    };
+    private int difficulty;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-		gameObject.transform.GetChild(0).gameObject.GetComponent<Text>().text = "Coding for the project";
-		display = gameObject.transform.GetChild(1).gameObject.GetComponent<Text>();
-		display.text = "";
+        gameObject.transform.GetChild(0).gameObject.GetComponent<Text>().text = "Coding for the project";
+        display = gameObject.transform.GetChild(1).gameObject.GetComponent<Text>();
+        display.text = "";
         ResetWordList();
         progress = GameObject.Find("Canvas/ProgressBar/Foreground").GetComponent<Progress>();
-	}
+    }
 
-	public void Reset(int difficulty)
+    public void Reset(int difficulty)
     {
-		Start();
-		over = false;
-		gameObject.SetActive(true);
-		this.difficulty = difficulty;
+        Start();
+        over = false;
+        gameObject.SetActive(true);
+        this.difficulty = difficulty;
         ResetWordList();
-	}
+    }
 
 
     void ResetWordList()
@@ -90,39 +90,39 @@ public class Typer : MonoBehaviour
         }
     }
 
-	// Update is called once per frame
-	void Update()
+    // Update is called once per frame
+    void Update()
     {
-		if (!over)
+        if (!over)
         {
-			string word = to_type[0];
-			char letter = word[word_offset];
-			if (Input.GetKeyUp(letter.ToString()))
+            string word = to_type[0];
+            char letter = word[word_offset];
+            if (Input.GetKeyUp(letter.ToString()))
             {
-				word_offset += 1;
+                word_offset += 1;
 
-				if (word_offset == word.Length)
+                if (word_offset == word.Length)
                 {
-					word_offset = 0;
+                    word_offset = 0;
                     // list_offset += 1;
                     to_type.RemoveAt(0);
                     if (to_type.Count == 0)
-					// if (list_offset == to_type.Length)
+                    // if (list_offset == to_type.Length)
                     {
-						over = true;
-						gameObject.SetActive(false);
-					}
-				}
-			}
-			result = "";
+                        over = true;
+                        gameObject.SetActive(false);
+                    }
+                }
+            }
+            result = "";
             int maxWordPrinted = (to_type.Count < 10) ? to_type.Count : 10;
-			for (int i = 0; i < maxWordPrinted; i++)
+            for (int i = 0; i < maxWordPrinted; i++)
             {
-				result += to_type[i] + " ";
-			}
-			display.text = "> " + result.Substring(word_offset);
-		}
-	}
+                result += to_type[i] + " ";
+            }
+            display.text = "> " + result.Substring(word_offset);
+        }
+    }
 
     void OnDisable()
     {
